a3c68c694e75f61f530585fd5b2db7eee56ae14f
Kell <miqueiasdasilvabatista@gmail.com>
2025-11-21 17:01:37 -0300

    Criando tela inicial + geração de letras/palavras aleatórias

--- teste.py
+++ teste.py
@@ -1,7 +1,27 @@
+import random
+import string
+print("____________________________\nBem Vindo ao DashBoard\n____________________________\n")
+while True:
+    print("Qual funão deseja acessar?\nSelecionar banco de dados(1)\nVisualizar banco de dados(2)\n\
+          criar bancos de dados(5)")
+
+
+
+
+
+'''def letrasAleatorias(n):
+    linha = ''
+    for i in range(n):
+        letra = random.choice(string.ascii_letters)
+        linha += letra.upper()
+    return linha
+print(letrasAleatorias(5))'''
+
+'''
 lista = []
 with open("ex.txt", "r") as arquivo:
     for i in range(2):
         empresas = arquivo.readline()
         lista.append(empresas.split())
 print(lista)
-    
+'''
