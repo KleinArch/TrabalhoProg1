1939b76b9cbed758cae9e0038a34bf331e8a1934
Kell <miqueiasdasilvabatista@gmail.com>
2025-11-21 18:16:49 -0300

    Geração de dados, em inicio

--- teste.py
+++ teste.py
@@ -1,27 +1,25 @@
 import random
 import string
-print("____________________________\nBem Vindo ao DashBoard\n____________________________\n")
+'''print("____________________________\nBem Vindo ao DashBoard\n____________________________\n")
 while True:
     print("Qual funão deseja acessar?\nSelecionar banco de dados(1)\nVisualizar banco de dados(2)\n\
-          criar bancos de dados(5)")
+          Criar bancos de dados(3)\nVisualizar resultados(4)")'''
 
-
-
-
-
-'''def letrasAleatorias(n):
+def letrasAleatorias(n):
     linha = ''
     for i in range(n):
         letra = random.choice(string.ascii_letters)
         linha += letra.upper()
     return linha
-print(letrasAleatorias(5))'''
-
-'''
-lista = []
-with open("ex.txt", "r") as arquivo:
-    for i in range(2):
-        empresas = arquivo.readline()
-        lista.append(empresas.split())
-print(lista)
-'''
+def precosAleatorios():
+    saida = 0
+    inteiro = random.randint(1,1000)
+    decimal = random.randint(0,99)
+    saida = float(f'{inteiro}.{decimal}')
+    return saida
+def funcPequena():
+    with open("ex.txt", "w") as arquivo:
+        arquivo.write('')
+    for i in range(20):
+        with open("ex.txt", "a") as arquivo:
+            arquivo.write(f"{letrasAleatorias(5)},{precosAleatorios()}")
